dfc8bb24b176f135ece3371a9a52cc35f233f4a2
Vio IT <150695785+vioit22@users.noreply.github.com>
2024-06-23 12:49:49 +0300

    Add files via upload

---
+++ Future Calendar.py
@@ -0,0 +1,115 @@
+import calendar
+from logging import root
+root = input("Future Calendar")
+year1 = 2125
+year2 = 2126
+year3 = 2127
+year4 = 2128
+year5 = 2129
+year6 = 2130
+year7 = 2131
+year8 = 2132
+year9 = 2133
+year10= 2134
+year11= 2135
+year12= 2136
+year13= 2137
+year14= 2138
+year15= 2139
+year16= 2140
+year17= 2141
+year18= 2142
+year19= 2143
+year20= 2144
+year21= 2145
+year22= 2146
+year23= 2147
+year24= 2148
+year25= 2149
+year26= 2150
+year27= 2200
+year28= 2300
+year29= 2400
+year30= 2500
+year31= 3000
+year32= 4000
+year33= 5000
+year34= 6000
+year35= 7000
+year36= 8000
+year37= 9000
+year38= 10000
+year39= 100000
+year40= 1000000
+year41= 10000000
+year42= 100000000
+year43= 1000000000
+year44= 10000000000
+year45= 100000000000
+year46= 1000000000000
+year47= 10000000000000
+year48= 100000000000000
+year49= 1000000000000000
+year50= 10000000000000000
+year51= 100000000000000000
+year52= 1000000000000000000
+year53= 10000000000000000000
+year54= 100000000000000000000
+year55= 1000000000000000000000
+print(calendar.calendar(int(year1)))
+print(calendar.calendar(int(year2)))
+print(calendar.calendar(int(year3)))
+print(calendar.calendar(int(year4)))
+print(calendar.calendar(int(year5)))
+print(calendar.calendar(int(year6)))
+print(calendar.calendar(int(year7)))
+print(calendar.calendar(int(year8)))
+print(calendar.calendar(int(year9)))
+print(calendar.calendar(int(year10)))
+print(calendar.calendar(int(year11)))
+print(calendar.calendar(int(year12)))
+print(calendar.calendar(int(year13)))
+print(calendar.calendar(int(year14)))
+print(calendar.calendar(int(year15)))
+print(calendar.calendar(int(year16)))
+print(calendar.calendar(int(year17)))
+print(calendar.calendar(int(year18)))
+print(calendar.calendar(int(year19)))
+print(calendar.calendar(int(year20)))
+print(calendar.calendar(int(year21)))
+print(calendar.calendar(int(year22)))
+print(calendar.calendar(int(year23)))
+print(calendar.calendar(int(year24)))
+print(calendar.calendar(int(year25)))
+print(calendar.calendar(int(year26)))
+print(calendar.calendar(int(year27)))
+print(calendar.calendar(int(year28)))
+print(calendar.calendar(int(year29)))
+print(calendar.calendar(int(year30)))
+print(calendar.calendar(int(year31)))
+print(calendar.calendar(int(year32)))
+print(calendar.calendar(int(year33)))
+print(calendar.calendar(int(year34)))
+print(calendar.calendar(int(year35)))
+print(calendar.calendar(int(year36)))
+print(calendar.calendar(int(year37)))
+print(calendar.calendar(int(year38)))
+print(calendar.calendar(int(year39)))
+print(calendar.calendar(int(year40)))
+print(calendar.calendar(int(year41)))
+print(calendar.calendar(int(year42)))
+print(calendar.calendar(int(year43)))
+print(calendar.calendar(int(year44)))
+print(calendar.calendar(int(year45)))
+print(calendar.calendar(int(year46)))
+print(calendar.calendar(int(year47)))
+print(calendar.calendar(int(year48)))
+print(calendar.calendar(int(year49)))
+print(calendar.calendar(int(year50)))
+print(calendar.calendar(int(year51)))
+print(calendar.calendar(int(year52)))
+print(calendar.calendar(int(year53)))
+print(calendar.calendar(int(year54)))
+print(calendar.calendar(int(year55)))
+input("Search the year, month and date scrolling with mouse! Future is now! Hit Enter to continue!")
+input("Hit Enter to exit!")
